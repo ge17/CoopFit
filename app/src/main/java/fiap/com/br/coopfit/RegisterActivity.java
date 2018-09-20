ac8bf10702e29524838953d6c7a58d83f5a488f8
George Luis <george.luis@live.com>
2018-09-19 22:40:53 -0300

    Commit 1.0

--- app/src/main/java/fiap/com/br/coopfit/RegisterActivity.java
+++ app/src/main/java/fiap/com/br/coopfit/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import fiap.com.br.coopfit.dao.CoopFitDB;
 import fiap.com.br.coopfit.to.Pessoa;
@@ -23,11 +24,19 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void saveChanges(View view) {
-        Pessoa p = new Pessoa();
-        p.setEmail(txtEmail.getText().toString());
-        p.setSenha(txtSenha.getText().toString());
 
-        CoopFitDB db = new CoopFitDB(this);
-        db.insertPessoa(p);
+        try {
+            Pessoa p = new Pessoa();
+            p.setEmail(txtEmail.getText().toString());
+            p.setSenha(txtSenha.getText().toString());
+
+            CoopFitDB db = new CoopFitDB(this);
+            db.insertPessoa(p);
+            db.close();
+            Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show();
+            finish();
+        }catch (Exception e){
+
+        }
     }
 }
